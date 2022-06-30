e196bd53c3d7109de0563178c34d5280a0cf812d
JeongIn <devforin@gmail.com>
2022-06-30 22:17:23 +0900

    SpringBoot Project, use gradle

---
+++ src/main/java/com/CamperLand/CT/CtApplication.java
@@ -0,0 +1,13 @@
+package com.CamperLand.CT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtApplication.class, args);
+	}
+
+}
