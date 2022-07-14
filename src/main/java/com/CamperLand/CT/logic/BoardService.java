f4b3c79e67a66ed4606ee5223dbf0074c2034552
JeongIn <devforin@gmail.com>
2022-07-14 17:44:06 +0900

    게시판 글쓰기만 완료

--- src/main/java/com/CamperLand/CT/logic/BoardService.java
+++ src/main/java/com/CamperLand/CT/logic/BoardService.java
@@ -2,13 +2,11 @@ package com.CamperLand.CT.logic;
 
 import com.CamperLand.CT.mapper.BoardMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@Repository
 @RequiredArgsConstructor
 public class BoardService {
     private final BoardMapper boardMapper;
